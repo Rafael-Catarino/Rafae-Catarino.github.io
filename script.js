940fba33a2b2ade062c60e43cd0c7e7bc9da3feb
rafael <rafaelscatarino@hotmail.com>
2022-07-16 18:46:36 -0300

    adiciona header responsivo

---
+++ script.js
@@ -0,0 +1,10 @@
+const headerButton = document.querySelector("#header-button");
+
+headerButton.addEventListener('click', () => {
+  const navHeader = document.querySelector('#nav-header');
+  if(navHeader.classList == 'nav-header-disappear') {
+    navHeader.classList.remove('nav-header-disappear');
+  } else {
+    navHeader.classList = 'nav-header-disappear';
+  }
+});
