cb865d966baef3d3f6945af0bb093066228dbf57
rafael <rafaelscatarino@hotmail.com>
2022-07-19 17:08:24 -0300

    adicionando fotos, projetos, melhorando a responsividade...

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const headerButton = document.querySelector("#header-button");
 
 headerButton.addEventListener('click', () => {
   const navHeader = document.querySelector('#nav-header');
-  if(navHeader.classList == 'nav-header-disappear') {
+  if (navHeader.classList == 'nav-header-disappear') {
     navHeader.classList.remove('nav-header-disappear');
   } else {
     navHeader.classList = 'nav-header-disappear';
